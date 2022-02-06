30adf6875570f86c1eb654efd05073e6c02520ba
inrir <kimms5617@naver.com>
2022-02-06 17:55:22 +0900

    codeup0206을 통해서 해당 날짜에 한 것들을 모아두었습니다.

---
+++ src/codeup0206.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class codeup0206 {
+    public static void main(String[] args) throws IOException {
+
+        //code up 1029
+      /* double a = Double.parseDouble(bf.readLine());
+        System.out.println(String.format("%.11f",a));*/
+
+        //code up 1030
+      /*  long a = Long.parseLong(bf.readLine());
+        System.out.println(a);*/
+
+        //code up 1031~1033
+       /* int a = Integer.parseInt(bf.readLine());
+//        System.out.println(String.format("%o",a));
+//        System.out.println(String.format("%x",a));
+        System.out.println(String.format("%X",a));*/
+
+        //code up 1034
+        /*int a = Integer.valueOf(bf.readLine(),8);
+        System.out.println(a);*/
+
+        //code up 1035
+        /*int a = Integer.valueOf(bf.readLine(),16);
+        System.out.println(Integer.toOctalString(a));*/
+
+        //code up 1036 문자 -> 정수
+        /*int a = bf.readLine().charAt(0);
+        System.out.println(a);*/
+        //code up 1037 정수 -> 아스키문자
+       /* int a = Integer.parseInt(bf.readLine());
+        char b = (char)a;
+        System.out.println(b);*/
+
+        //code up 1038 정수 2개 입력받고 합 출력 + 정수의 범위 고려하기!
+      /*  long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        System.out.println(a+b);*/
+
+        //code up 1039 정수 2개 입력받고 합 출력 + int의 범위를 넘어선다면?
+    /*   long a = Long.parseLong(st.nextToken());
+       long b = Long.parseLong(st.nextToken());
+        System.out.println(a+b);*/
+
+        // code up 1040
+        /*int a = Integer.parseInt(bf.readLine());
+        System.out.println(-a);*/
+
+        // code up 1041
+   /*     char a = bf.readLine().charAt(0);
+        System.out.println((char)(a+1));*/
+
+        // code up 1042 ~ 1043
+       /* int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+//        System.out.println(a/b);
+        System.out.println(a%b);*/
+        // code up 1044
+        /*long a = Long.parseLong(bf.readLine());
+        System.out.println(++a);*/
+        // code up 1045
+       /* int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(String.format("%.2f",(float)a/b));*/
+        // code up 1046
+       /* int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        long d = a+b+c;
+        double e = (double) d/3;
+        System.out.println(d);
+        System.out.println(String.format("%.1f",e));*/
+
+    }
+}
