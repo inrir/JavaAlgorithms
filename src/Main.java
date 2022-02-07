2e084a6614a41f814d8c3133983370a3a5c76983
inrir <kimms5617@naver.com>
2022-02-07 23:28:09 +0900

    code up 1058까지 했습니다!

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,7 +8,31 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        //code up 1047 비트연산자 사용하는 방법
+       /* int a = Integer.parseInt(bf.readLine());
+        System.out.println(String.format("%d",a<<1));*/
+
+        //code up 1048 ~ 1058
+//        int a = Integer.parseInt(st.nextToken());
+//        int b = Integer.parseInt(st.nextToken());
+//        boolean a = Boolean.valueOf(bf.readLine()).booleanValue();
+        boolean a = (Integer.parseInt(st.nextToken()) == 1);
+        boolean b = (Integer.parseInt(st.nextToken()) == 1);
+//        System.out.println(String.format("%d",a<<b));
+//        System.out.println(String.format("%d", (a>b)? 1:0));
+//        System.out.println(String.format("%d", (a==b)? 1:0));
+//        System.out.println(String.format("%d", (a<=b)? 1:0));
+//        System.out.println(String.format("%d", (a!=b)? 1:0));
+//        System.out.println((a&&b)? 1:0);
+
+//        System.out.println((a&&b)? 1:0);
+//        System.out.println((a||b)? 1:0);
+//        System.out.println(((a&&!b)||(!a&&b))? 1:0);
+//        System.out.println((a==b)? 1:0);
+        System.out.println(((a==b)&&(!a))? 1:0);
+
+        //code up 10
 
     }
 
